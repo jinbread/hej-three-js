9dc7e5ee124a9d301d0b77f09459688387dd0a52
Jinjae Lee <jinjae.lee@icloud.com>
2021-12-19 00:42:19 +0900

    lesson 05 updates

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import * as THREE from "three";
+import { EventDispatcher } from "three";
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -10,17 +11,64 @@ const scene = new THREE.Scene();
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-mesh.position.x = 0.7;
-mesh.position.y = -0.6;
-mesh.position.z = 1;
-scene.add(mesh);
+const group = new THREE.Group();
+group.position.y = 1;
+group.scale.x = 0.5;
+group.rotation.y = 1;
+scene.add(group);
 
-// console.log(mesh.position.length());
-// console.log(mesh.position.distanceTo(new THREE.Vector3(0, 1, 2)));
-// mesh.position.normalize();
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "red" })
+);
+
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "green" })
+);
+
+cube2.position.set(-2, 0, 1);
+
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "blue" })
+);
+
+cube3.position.set(2, 0, 1);
+
+group.add(cube3);
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const mesh = new THREE.Mesh(geometry, material);
+// // mesh.position.x = 0.7;
+// // mesh.position.y = -0.6;
+// // mesh.position.z = 1;
+// mesh.position.set(0.7, -0.6, 1);
+
+// scene.add(mesh);
+
+// // console.log(mesh.position.length());
+// // console.log(mesh.position.distanceTo(new THREE.Vector3(0, 1, 2)));
+// // mesh.position.normalize();
+
+// // Scale
+// // mesh.scale.x = 2;
+// // mesh.scale.y = .5;
+// mesh.scale.set(2, 0.5, 0.5);
+
+// // Rotation 순서가 영향을 줌. Reorder 사용
+// mesh.rotation.reorder("YXZ");
+// mesh.rotation.y = Math.PI * 0.5;
+// mesh.rotation.x = Math.PI * 0.25;
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
 
 /**
  * Sizes
@@ -35,7 +83,12 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
+// camera.position.y = 1;
+// camera.position.x = 1;
 scene.add(camera);
+
+// camera.lookAt(new THREE.Vector3(3, 0, 0));
+// camera.lookAt(mesh.position);
 
 /**
  * Renderer
